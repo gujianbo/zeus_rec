c0681dbd56db198dd8cd6fefec2c2332e1673a62
gujianbo <gujianbobo@gmail.com>
2022-12-07 13:45:22 +0800

    heap

--- models/swing.py
+++ models/swing.py
@@ -8,21 +8,26 @@ from pathlib import Path
 import random
 import pandas as pd
 from copy import deepcopy
-from utils.lossy_counting import Lossy_Counting
+from utils.heap import Heap
 import logging
 from itertools import combinations
+import hashlib
 
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(filename)s[line:%(lineno)d]- %(message)s"
 logging.basicConfig(filename=config.log_file, level=logging.DEBUG, format=LOG_FORMAT)
 
 
-def gen_item_pair(input_file):
+def gen_item_pair(input_file, output_file):
     # session_chunks = pd.read_json(input_file, lines=True, chunksize=100000)
     session_item = dict()
     item_session = dict()
-    pair_dict = dict()
+    # pair_dict = dict()
     # item_pairs = set()
-    # fdout = open(output_file + ".pair.tmp", "w")
+    pair_path = Path(output_file).parent.joinpath("tmp")
+    fdout = []
+    for i in range(10):
+        fd = open(str(pair_path)+"/pair_"+str(i), "w")
+        fdout.append(fd)
     idx = 0
     with open(input_file, "r") as f:
         for line in tqdm(f, desc="gen_item_pair"):
@@ -36,77 +41,89 @@ def gen_item_pair(input_file):
                 item_session[aid].add(session_id)
             pair_list = list(combinations(click_aid, 2))
 
-            for pair in pair_list:
-                if pair[0] == pair[1]:
-                    continue
-                pair_dict.setdefault(pair[0], Lossy_Counting(pair[0], 10000))
-                pair_dict.setdefault(pair[1], Lossy_Counting(pair[1], 10000))
-                pair_dict[pair[0]].enter_num(pair[1])
-                pair_dict[pair[1]].enter_num(pair[0])
-
             # for pair in pair_list:
-            #     if pair[0] > pair[1]:
-            #         pair_str = str(pair[0]) + "," + str(pair[1])
-            #     elif pair[0] < pair[1]:
-            #         pair_str = str(pair[1]) + "," + str(pair[0])
-            #     else:
+            #     if pair[0] == pair[1]:
             #         continue
-            #     fdout.write(pair_str+"\n")
+            #     pair_dict.setdefault(pair[0], Lossy_Counting(pair[0], 10000))
+            #     pair_dict.setdefault(pair[1], Lossy_Counting(pair[1], 10000))
+            #     pair_dict[pair[0]].enter_num(pair[1])
+            #     pair_dict[pair[1]].enter_num(pair[0])
+
+            for pair in pair_list:
+                if pair[0] > pair[1]:
+                    pair_str = str(pair[0]) + "," + str(pair[1])
+                elif pair[0] < pair[1]:
+                    pair_str = str(pair[1]) + "," + str(pair[0])
+                else:
+                    continue
+                hash_code = int(hashlib.md5((pair_str + str(config.seed)).encode('utf8')).hexdigest()[0:10], 16) % 10
+                fdout[hash_code].write(pair_str+"\n")
             idx += 1
-            if idx >= 1000000:
+            if idx >= 100000:
                 break
-    # fdout.close()
-    logging.info("Loop train file done!")
-    pair_set = set()
-    for key in pair_dict.keys():
-        sort_key = pair_dict[key].topk(5000)
-        for (r_key, count) in sort_key:
-            if key > r_key:
-                pair_set.add(str(r_key) + "," + str(key))
-            elif key < r_key:
-                pair_set.add(str(key) + "," + str(r_key))
+    for fd in fdout:
+        fd.close()
+    # logging.info("Loop train file done!")
+    # pair_set = set()
+    # for key in pair_dict.keys():
+    #     sort_key = pair_dict[key].topk(5000)
+    #     for (r_key, count) in sort_key:
+    #         if key > r_key:
+    #             pair_set.add(str(r_key) + "," + str(key))
+    #         elif key < r_key:
+    #             pair_set.add(str(key) + "," + str(r_key))
+    #
+    # logging.info("item pairs length：{}".format(len(pair_set)))
+    return session_item, item_session
 
-    logging.info("item pairs length：{}".format(len(pair_set)))
-    return session_item, item_session, pair_set
 
-
-def calc_simlarity(item_pairs, session_item, item_session, output_file, alpha=1.0, session_num_threhold=200):
+def calc_simlarity(session_item, item_session, output_file, alpha=1.0, session_num_threhold=200):
     item_sim_dict = dict()
-    logging.info("item pairs length：{}".format(len(item_pairs)))
-    # fdout = open(output_file, "w")
+    # logging.info("item pairs length：{}".format(len(item_pairs)))
+    fdout = open(output_file, "w")
+    pair_path = Path(output_file).parent.joinpath("tmp")
 
-    # with open(pair_file, "r") as f:
-    for item_pair in tqdm(item_pairs, desc="calculate similarities"):
-        pair_str = item_pair.strip()
-        item_i, item_j = pair_str.split(",")
-        item_i = int(item_i)
-        item_j = int(item_j)
-        common_sessions = item_session[item_i] & item_session[item_j]
-        # 采个样，防止太多，撑爆内存
-        if len(common_sessions) > session_num_threhold:
-            common_sessions = random.sample(common_sessions, session_num_threhold)
-        session_pairs = list(combinations(common_sessions, 2))
-        result = 0.0
-        for (user_u, user_v) in session_pairs:
-            result += 1 / (alpha + list(session_item[user_u] & session_item[user_v]).__len__())
-        item_sim_dict.setdefault(item_i, dict())
-        item_sim_dict[item_i][item_j] = result
-        item_sim_dict.setdefault(item_j, dict())
-        item_sim_dict[item_j][item_i] = result
-        # fdout.write(str(item_i)+","+str(item_j)+","+str(result))
+    for i in range(10):
+        pair_file = str(pair_path) + "/pair_" + str(i) + ".uniq"
+        with open(pair_file, "r") as f:
+        # for item_pair in tqdm(item_pairs, desc="calculate similarities"):
+            for item_pair in tqdm(f, desc="calc_simlarity:"+pair_file):
+                pair_str = item_pair.strip()
+                item_i, item_j = pair_str.split(",")
+                item_i = int(item_i)
+                item_j = int(item_j)
+                common_sessions = item_session[item_i] & item_session[item_j]
+                # 采个样，防止太多，撑爆内存
+                if len(common_sessions) > session_num_threhold:
+                    common_sessions = random.sample(common_sessions, session_num_threhold)
+                session_pairs = list(combinations(common_sessions, 2))
+                result = 0.0
+                for (user_u, user_v) in session_pairs:
+                    result += 1 / (alpha + list(session_item[user_u] & session_item[user_v]).__len__())
+                item_sim_dict.setdefault(item_i, Heap(item_i, 100))
+                item_sim_dict[item_i].enter_item([item_j, result])
+                item_sim_dict.setdefault(item_j, Heap(item_j, 100))
+                item_sim_dict[item_j].enter_item([item_i, result])
+                # fdout.write(str(item_i)+","+str(item_j)+","+str(result))
     logging.info("Calculate similarity finished!")
     # fdout.close()
     return item_sim_dict
 
 def uniq_pair(input_file):
     logging.info("start uniq pair file")
-    os.system("sort " + input_file + ".pair.tmp | uniq > " + input_file+".uniq")
-    logging.info("uniq pair file finished!")
+    for i in range(10):
+        pair_path = Path(input_file).parent.joinpath("tmp")
+        pair_file = str(pair_path)+"/pair_"+str(i)
+        logging.info("To uniq file:" + pair_file)
+        os.system("sort " + pair_file + ".pair.tmp | uniq > " + pair_file+".uniq")
+        logging.info("Uniq file:" + pair_file + "done!")
+    logging.info("Uniq pair file finished!")
+
 
 def output_sim_file(item_sim_dict, out_path, top_k=100):
     fd = open(out_path, "w")
     for item, sim_items in item_sim_dict.items():
-        sim_score = sorted(sim_items.items(), key=lambda k: k[1], reverse=True)[:top_k]
+        sim_score = sim_items.top_items()
         for (item_j, score) in sim_score:
             fd.write(str(item) + "\t" + str(item_j) + "\t" + str(score) + "\n")
     fd.close()
@@ -114,7 +131,7 @@ def output_sim_file(item_sim_dict, out_path, top_k=100):
 
 def main():
     session_item, item_session, item_pair = gen_item_pair(config.input_file)
-    # uniq_pair(config.output_file)
+    uniq_pair(config.output_file)
     item_sim_dict = calc_simlarity(item_pair, session_item, item_session)
     del session_item, item_session
     output_sim_file(item_sim_dict, config.output_file)
---
+++ utils/heap.py
@@ -0,0 +1,100 @@
+from copy import deepcopy
+import numpy as np
+import math
+
+
+class Heap:
+    def __init__(self, anchor, capacity):
+        self.anchor = anchor
+        self.capacity = capacity
+        self.data = []
+
+    def enter_item(self, item):
+        if len(self.data) >= self.capacity and item[1] > self.data[0][1]:
+            self.data[0] = item
+            idx = 0
+            while idx < len(self.data):
+                left = 2 * idx + 1
+                right = 2 * idx + 2
+                if right < len(self.data):
+                    if self.data[left][1] >= self.data[idx][1] and self.data[right][1] >= self.data[idx][1]:
+                        break
+                    elif self.data[left][1] >= self.data[right][1]:
+                        tmp = self.data[right]
+                        self.data[right] = self.data[idx]
+                        self.data[idx] = tmp
+                        idx = right
+                    else:
+                        tmp = self.data[left]
+                        self.data[left] = self.data[idx]
+                        self.data[idx] = tmp
+                        idx = left
+                elif left < len(self.data):
+                    if self.data[left][1] >= self.data[idx][1]:
+                        break
+                    else:
+                        tmp = self.data[left]
+                        self.data[left] = self.data[idx]
+                        self.data[idx] = tmp
+                        idx = left
+                else:
+                    break
+
+        elif len(self.data) < self.capacity:
+            self.data.append(item)
+            idx = len(self.data) - 1
+            while idx >= 0:
+                parent = math.floor((idx - 1) / 2)
+                if self.data[idx][1] >= self.data[parent][1]:
+                    break
+                tmp = self.data[parent]
+                self.data[parent] = self.data[idx]
+                self.data[idx] = tmp
+                idx = parent
+
+    def top_items(self):
+        top_item = []
+        while len(self.data) > 0:
+            top_item.append(self.data[0])
+            data_len = len(self.data)
+            self.data[0] = deepcopy(self.data[data_len-1])
+            self.data.pop()
+
+            idx = 0
+            while idx < len(self.data):
+                left = 2 * idx + 1
+                right = 2 * idx + 2
+                if right < len(self.data):
+                    if self.data[left][1] >= self.data[idx][1] and self.data[right][1] >= self.data[idx][1]:
+                        break
+                    elif self.data[left][1] >= self.data[right][1]:
+                        tmp = self.data[right]
+                        self.data[right] = self.data[idx]
+                        self.data[idx] = tmp
+                        idx = right
+                    else:
+                        tmp = self.data[left]
+                        self.data[left] = self.data[idx]
+                        self.data[idx] = tmp
+                        idx = left
+                elif left < len(self.data):
+                    if self.data[left][1] >= self.data[idx][1]:
+                        break
+                    else:
+                        tmp = self.data[left]
+                        self.data[left] = self.data[idx]
+                        self.data[idx] = tmp
+                        idx = left
+                else:
+                    break
+        return top_item
+
+
+if __name__ == "__main__":
+    h = Heap(1, 500)
+    for i in range(200000):
+        rand = int(np.random.normal(50, 20))
+        h.enter_item([i, rand])
+    top_items = h.top_items()
+    for item in top_items:
+        print(item)
