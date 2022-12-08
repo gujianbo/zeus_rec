b8a9346b9fa71074b903391d302a51d390a91707
gujianbo <gujianbobo@gmail.com>
2022-12-08 09:09:29 +0800

    filter

--- models/recall/swing.py
+++ models/recall/swing.py
@@ -33,7 +33,7 @@ def gen_item_pair(input_file, output_file, debug=0):
             session_id = session['session']
             click_aid = set([event['aid'] for event in session['events'] if event["type"] == "clicks"])
             # print(click_aid)
-            if len(click_aid) <= 1:
+            if len(click_aid) <= 3:
                 continue
             session_item[session_id] = click_aid
             for aid in click_aid:
